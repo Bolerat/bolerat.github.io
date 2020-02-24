6d80890f4d5626ed2fab0e9eafcc12dec7f5c9bf
Bolerat <bolerat@outlook.com>
2020-02-25 02:31:18 +0800

    Site updated: 2020-02-25 02:31:17

--- js/APlayer_config.js
+++ js/APlayer_config.js
@@ -1,7 +1,7 @@
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
     fixed: true,
-    autoplay: false,
+    autoplay: true,
     theme: '#b7daff',
     loop: 'all',
     order: 'list',
