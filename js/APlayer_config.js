dc759b8fcd21d4e98a2bb4d057304c76746a78cc
Bolerat <bolerat@outlook.com>
2020-02-25 02:21:25 +0800

    Site updated: 2020-02-25 02:21:22

---
+++ js/APlayer_config.js
@@ -0,0 +1,24 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    theme: '#b7daff',
+    loop: 'all',
+    order: 'list',
+    preload: 'auto',
+    volume: 0.5,
+    lrcType: 3,
+    mutex: true,
+    listFolded: false,
+
+    audio: [
+		{
+            name: '随机',
+            artist: 'Artist',
+            lrc: '',
+            cover: 'https://bolerat.github.io/assets/avatar.png',
+            url: 'https://jx.618g.com/kuwo.php'
+                  },
+		 
+        ]
+});
